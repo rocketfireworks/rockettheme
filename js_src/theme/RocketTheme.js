ea72a6c33290962bf862c76ea0fc959605359639
Jade <jade@user1.net>
2021-06-04 13:35:05 -0400

    Added initCartBonus

--- js_src/theme/RocketTheme.js
+++ js_src/theme/RocketTheme.js
@@ -35,7 +35,7 @@ export class RocketTheme {
             originalShopifyOnCartUpdate(cart, form);
             this.bonusRewards.updateCartData();
           }
-          this.bonusRewards.updateCartData();
+          this.bonusRewards.initCartBonus();
         }
       }
     }, 500);
