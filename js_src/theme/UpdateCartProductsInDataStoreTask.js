6f1427c0f3a916ffedb9b4a6340feaa87ab6d3e6
Colin Moock <colin@megaphonetv.com>
2021-06-06 21:53:06 -0400

    -UpdateCartProductsInDataStoreTask: Add debug logging to draw attention to an invalid local cart state

--- js_src/theme/UpdateCartProductsInDataStoreTask.js
+++ js_src/theme/UpdateCartProductsInDataStoreTask.js
@@ -8,6 +8,18 @@ export class UpdateCartProductsInDataStoreTask extends TaskManager {
   
   constructor () {
     super('GET ALL PRODUCTS IN CART');
+
+    // DEBUGGING CODE:
+    // If productsInCart and the cart.items ever have different lengths, something has gone wrong,
+    // and the cause should be investigated.
+    this.on(COMPLETE, () => {
+      if (RocketTheme.globals.dataStore.productsInCart.length
+        !== RocketTheme.globals.dataStore.cart.items.length) {
+        console.warn('cart.items and productsInCart MISMATCH')
+        console.log('datastore.cart.items: ', RocketTheme.globals.dataStore.cart.items);
+        console.log('datastore.productsInCart: ', RocketTheme.globals.dataStore.productsInCart)
+      }
+    });
   }
 
   initTasks () {
