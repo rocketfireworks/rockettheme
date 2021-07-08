3ebdd8b702c81d646ea762e0af2565c33d0297aa
Jade <jade@user1.net>
2021-07-08 12:45:02 -0400

    Added page reload when rendered price is different from actual cart price

--- js_src/theme/QuickCartSubtotalVerifier.js
+++ js_src/theme/QuickCartSubtotalVerifier.js
@@ -13,19 +13,29 @@ export class QuickCartSubtotalVerifier {
     setTimeout(() => {
       let renderedPrice;
       let normalPriceSpan = document.querySelector('.tdf_price_normal .tdf_money.money');
-      let floorLocalCartTotal = Math.floor(RocketTheme.globals.dataStore.totalInCart);
+      let flooredLocalCartTotal = Math.floor(RocketTheme.globals.dataStore.totalInCart);
       if (notNil(normalPriceSpan)) {
         renderedPrice = parseFloat(normalPriceSpan.textContent.substr(1)) * 100;
         console.log('%% Rendered price: ', renderedPrice);
-        console.log('%% Floor cart total: ', floorLocalCartTotal);
+        console.log('%% Floored cart total: ', flooredLocalCartTotal);
       }
       console.log('%% Normal price span: ', normalPriceSpan);
   
-      if (Math.abs(floorLocalCartTotal - renderedPrice) > 1) {
-        console.log('%%%Subtotal are different!!!');
+      if (Math.abs(flooredLocalCartTotal - renderedPrice) > 1) {
+        console.log('~~~Subtotal are different!!! Floored Local Cart Total: ', flooredLocalCartTotal, 'Rendered subtotal: ', renderedPrice);
         
+        // Unresolvable mismatch between the client-side subtotal and the actual correct server-side subtotal,
+        // so there is no alternative but to reload. This very rare problem occurs when items are added to and
+        // removed from the cart in quick succession. It does not occur for products without a Selly discount. 
+        // But when a Selly discount is applied, the quantity of items in the cart as reported by the Shopify 
+        // AJAX API sometimes falls out of synchronization with the actual correct server-side quantity 
+        setTimeout(() => {
+          if (Math.abs(flooredLocalCartTotal - renderedPrice) > 1) {
+            location.reload();
+          }
+        }, 5000);
       } else {
-        console.log('%%Subtotal check okay');
+        console.log('~~~Subtotal check okay. Floored Local Cart Total: ', flooredLocalCartTotal, 'Rendered subtotal: ', renderedPrice);
       }
     }, 2000);
   }
